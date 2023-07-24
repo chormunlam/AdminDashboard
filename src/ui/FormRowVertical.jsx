e9a9ecbeb6a1bd2a2867d7fdad8b70513ee3e277
Chormun Lam <chormunlam@yahoo.com.hk>
2023-07-24 20:49:53 +0000

    i hate git

---
+++ src/ui/FormRowVertical.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+
+const StyledFormRow = styled.div`
+  display: flex;
+  flex-direction: columns;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 0.8rem;
+  padding: 1.2rem 0;
+
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+// eslint-disable-next-line react/prop-types
+function FormRowVertical({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
+
+export default FormRowVertical;
