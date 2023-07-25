34c8a24169d96c321e75b8f98818cf00f63a086f
Chormun Lam <chormunlam@yahoo.com.hk>
2023-07-25 18:58:52 +0000

    sign up user

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,18 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-//import { useSignup } from "./auseSignup";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  //const { signup, isLoading } = useSignup();
-  const { register, formState, getValues, handleSubmit, reset } = useForm();
-  const { errors } = formState;
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset, formState: { errors} }= useForm();
+  
+  //const { errors } = formState;
 
-  function onSubmit(data) {
- console.log(data);
+
+  function onSubmit({fullName, email, password}) {
+    signup({fullName, email, password},{onSettled: reset,})
+    
   }
+
+
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -23,8 +28,8 @@ function SignupForm() {
           type="text"
           id="fullName"
           //disabled={isLoading}
-          {...register("fullName", { required: "This field is required" })}
-        />
+          {...register("fullName", { required: "This field is required" })} />
+      
       </FormRow>
 
       <FormRow label="Email address" error={errors?.email?.message}>
