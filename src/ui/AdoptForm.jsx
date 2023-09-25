33bb8bdc109e39e4985555f315da9ab8e1ec062c
Chormun Lam <chormunlam@yahoo.com.hk>
2023-09-25 23:53:21 +0000

    change form

--- src/ui/AdoptForm.jsx
+++ src/ui/AdoptForm.jsx
@@ -85,6 +85,82 @@ function AdoptForm() {
         What are some reasons you would relinquish this cat back to New York
         Meow Sanctuary? <Input type="text" />
       </Label>
+      {/* Previous and Current Pet Information */}
+      <h2>PREVIOUS AND CURRENT PET INFORMATION</h2>
+      <Label>
+        Have you ever had a pet: Run Away, Get hit by a car, Die in your care, Kept as an outdoor pet? If so, please explain:
+        <Input type="text" />
+      </Label>
+      <Label>
+        Have you ever surrendered any pet to a private rescue or individual? If yes, please explain the circumstance:
+        <Input type="text" />
+      </Label>
+      {/* Current Pets Information */}
+      <h3>Current Pets:</h3>
+      <Label>
+        Breed: <Input type="text" />
+      </Label>
+      <Label>
+        Age: <Input type="text" />
+      </Label>
+      <Label>
+        Gender M/F: <Input type="text" />
+      </Label>
+      <Label>
+        Altered Y/N: <Input type="text" />
+      </Label>
+      <Label>
+        How long have you owned? Inside/outside/both? <Input type="text" />
+      </Label>
+      <Label>
+        Are your current animals up to date on vaccines? <Input type="checkbox" />
+      </Label>
+      {/* Previous Pets Information */}
+      <h3>Previous Pets:</h3>
+      <Label>
+        Breed: <Input type="text" />
+      </Label>
+      <Label>
+        Age: <Input type="text" />
+      </Label>
+      <Label>
+        Gender M/F: <Input type="text" />
+      </Label>
+      <Label>
+        Altered Y/N: <Input type="text" />
+      </Label>
+      <Label>
+        What happened to the animal? <Input type="text" />
+      </Label>
+      <Label>
+        Prepared for vet expenses? <Input type="checkbox" />
+      </Label>
+      <Label>
+        Who is your veterinarian? <Input type="text" />
+      </Label>
+      <Label>
+        Other vets with your pets records? Provide name and contact:
+        <Input type="text" />
+      </Label>
+      {/* Household Info */}
+      <h2>HOUSEHOLD INFORMATION</h2>
+      <Label>Name and age: <Input type="text" /></Label>
+      <Label>Relationship: <Input type="text" /></Label>
+      <Label>Name and age: <Input type="text" /></Label>
+      <Label>Relationship: <Input type="text" /></Label>
+
+      <Label>
+        Anyone has allergies to animals? If YES, on medication?
+        <Input type="text" />
+      </Label>
+      <Label>
+        Anyone convicted of animal cruelty, neglect, or abandonment?
+        <Input type="checkbox" />
+      </Label>
+
+
+      <button type="submit">Submit</button>
+
     </form>
   );
 }
